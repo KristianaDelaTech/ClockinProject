0982c52c6888ab26df1ccfc102a38479f78bed65
Kristiana <k.trupja@reply.de>
2025-06-03 15:10:20 +0200

    update bottom bar

--- src/app/developer/components/calendar/BottomBar.tsx
+++ src/app/developer/components/calendar/BottomBar.tsx
@@ -1,44 +1,45 @@
-"use client";
-import { useEffect, useMemo, useState } from "react";
 import { useCalendar } from "@/app/context/CalendarContext";
-import { getDaysInMonth, isWeekend } from "@/app/utils/dateUtils";
 import { useWorkHours } from "@/app/context/WorkHoursContext";
 import { usePathname } from "next/navigation";
+import { getDaysInMonth, isWeekend } from "@/app/utils/dateUtils";
+import { useDayHoliday } from "@/app/hooks/useDayHoliday";
+import { useMemo, useState, useEffect } from "react";
 
 export default function BottomBar() {
   const { month, year } = useCalendar();
-  const [days, setDays] = useState<string[]>([]);
   const { getTotalHoursForDay } = useWorkHours();
-const pathname = usePathname();
-
-  const userId = useMemo(() => {
-    const segments = pathname?.split("/") || [];
-    return segments[2] || "";
-  }, [pathname]);
+  const pathname = usePathname();
+  const userId = useMemo(() => pathname?.split("/")[2] || "", [pathname]);
+  const [days, setDays] = useState<string[]>([]);
 
   useEffect(() => {
-    const daysArray = getDaysInMonth(year, month);
-    setDays(daysArray);
+    setDays(getDaysInMonth(year, month));
   }, [month, year]);
 
   return (
     <div className="flex items-center gap-1">
       {days.map((day) => {
-        const weekendClass = isWeekend(year, month, parseInt(day))
-          ? "border-red-400"
-          : "";
-        const formattedDate = `${year}-${String(month + 1).padStart(
-          2,
-          "0"
-        )}-${String(day).padStart(2, "0")}`;
+        const dayNumber = parseInt(day);
+        const formattedDate = `${year}-${String(month + 1).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
         const totalHours = getTotalHoursForDay(formattedDate, userId);
-        const bgColor = totalHours > 0 ? "bg-blue-100" : "bg-white";
+        const isWeekendDay = isWeekend(year, month, dayNumber);
+
+        const { isHoliday } = useDayHoliday(year, month, dayNumber);
+
+        let bgColor = totalHours > 0 ? "bg-blue-100" : "bg-white";
+
+        let borderColor = "border-gray-300";
+        if (isHoliday) {
+          borderColor = "border-green-400";
+        } else if (isWeekendDay) {
+          borderColor = "border-red-400";
+        }
 
         return (
           <div
             key={day}
             title={formattedDate}
-            className={`border-t-5 border-gray-300 w-8 h-6 flex justify-center items-center text-xs pt-1 ${weekendClass} ${bgColor}`}
+            className={`border-t-5 ${borderColor} w-8 h-6 flex justify-center items-center text-xs pt-1 ${bgColor}`}
           >
             {totalHours.toFixed(2)}
           </div>
